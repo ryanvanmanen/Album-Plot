d9068f1732ed9400099a9b6f950a8c1835530f35
ryanvanmanen <ryanpvanmanen@gmail.com>
2022-10-25 21:39:57 -0400

    added scatter plot

--- Album_Plot.R
+++ Album_Plot.R
@@ -1,6 +1,8 @@
 library(XML)
 library(xml2)
 library(methods)
+library(plotly)
+library(ggplot2)
 
 ##Order and manipulate album list
 albums <- read.csv(file="/Users/ryanvanmanen/Documents/Github/Album-Plot/Album_Data.csv")
@@ -14,9 +16,7 @@ ordered_albums <- ordered_albums[!is.na(ordered_albums$Discovered),]
 #Write ordered dataframe to a csv and put it in specified directory location
 write.csv(ordered_albums,file = "/Users/ryanvanmanen/Documents/Github/Album-Plot/ordered_albums.csv")
 
-View(ordered_albums)
-
-
+#View(ordered_albums)
 
 #Change Column Names with setNames
 #ordered_albums <-setNames(ordered_albums, c("Number","Album", "Artist","Released","Discovered"))
@@ -27,3 +27,19 @@ View(ordered_albums)
 #Label points on scatter plot
 #text(ordered_albums$Released, ordered_albums$Discovered, labels = ordered_albums$Album)
 
+#Create scatter plot with hover over points for labels
+
+fig <- plot_ly( type ='scatter', mode = 'markers' ) 
+
+fig <- fig %>%
+  add_trace(
+    x = ordered_albums$Released, 
+    y = ordered_albums$Discovered,
+    text = ordered_albums$Album,
+    hoverinfo = 'text',
+    marker = list(color='green'),
+    showlegend = F
+
+  )
+  
+fig
