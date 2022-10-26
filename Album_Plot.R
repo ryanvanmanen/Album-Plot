d9eadda948c805f4e55b53b366921370cdae3d19
ryanvanmanen <ryanpvanmanen@gmail.com>
2022-10-25 20:25:20 -0400

    update

--- Album_Plot.R
+++ Album_Plot.R
@@ -2,25 +2,24 @@ library(XML)
 library(xml2)
 library(methods)
 
-##XML conversion
-#itunes <- xmlToDataFrame('/Users/ryanvanmanen/Music/iTunes Music Library.xml')
-
-#View(itunes)
-
 ##Order and manipulate album list
 albums <- read.csv(file="/Users/ryanvanmanen/Documents/Github/Album-Plot/Album_Data.csv")
 
 #Create variable containing ordered dataframe
 ordered_albums <-albums[order(albums$Released, decreasing = FALSE), ] 
 
-#Change Column Names with setNames
-#ordered_albums <-setNames(ordered_albums, c("Number","Album", "Artist","Released","Discovered"))
-  
+#Remove rows with NA in Discovered column
+ordered_albums <- ordered_albums[!is.na(ordered_albums$Discovered),]
 
 #Write ordered dataframe to a csv and put it in specified directory location
 write.csv(ordered_albums,file = "/Users/ryanvanmanen/Documents/Github/Album-Plot/ordered_albums.csv")
 
 View(ordered_albums)
+
+
+
+#Change Column Names with setNames
+#ordered_albums <-setNames(ordered_albums, c("Number","Album", "Artist","Released","Discovered"))
 
 #Plot Release Date versus Discovery Date
 #plot.default(ordered_albums$Released,ordered_albums$Discovered)
